50a27d61dbe0e42d226f37907541412158f70eb0
unknown <srroy119@gmail.com>
2022-08-06 19:56:39 +0530

    2nd commit

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,10 +2,10 @@ import { useState } from "react";
 import Popup from "../components/Popup";
 import Sell from "../components/Sell";
 
-const dashboard = () => {
+const Dashboard = () => {
   const [openPopup,setOpenPopup] = useState(false);
   return (<>
-    <div onClick={()=>setOpenPopup(true)} className="cursor-pointer">dashboard</div>
+    <div onClick={()=>setOpenPopup(true)} className="cursor-pointer">Dashboard</div>
     <Popup openPopup={openPopup} setOpenPopup={setOpenPopup} >
         <Sell/>
     </Popup>
@@ -13,4 +13,4 @@ const dashboard = () => {
   )
 }
 
-export default dashboard;
+export default Dashboard;
